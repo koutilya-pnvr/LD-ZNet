d488061d365131fd6e9d4f297df25734914d2038
Koutilya <koutilya40192@gmail.com>
2023-06-02 19:18:07 -0400

    Initial code push

---
+++ ldm_seg/modules/image_degradation/__init__.py
@@ -0,0 +1,2 @@
+from ldm_seg.modules.image_degradation.bsrgan import degradation_bsrgan_variant as degradation_fn_bsr
+from ldm_seg.modules.image_degradation.bsrgan_light import degradation_bsrgan_variant as degradation_fn_bsr_light
---
+++ ldm_seg/modules/losses/__init__.py
@@ -0,0 +1,1 @@
+from ldm_seg.modules.losses.contperceptual import LPIPSWithDiscriminator
